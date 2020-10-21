b97baf9ad87a45336d5587c10d2be8524accc5ca
HDKnb <rlarhkr81@naver.com>
2020-10-21 12:22:12 +0900

    add new project

---
+++ mybatis/src/main/java/practice/mybatis/entity/members.java
@@ -0,0 +1,13 @@
+package practice.mybatis.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class members {
+
+    long m_num;
+    String name;
+    int age;
+}
---
+++ mybatis/src/main/java/practice/mybatis/entity/team.java
@@ -0,0 +1,7 @@
+package practice.mybatis.entity;
+
+public class team {
+
+    long m_num;
+    String team;
+}
